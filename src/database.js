ce09c826661ef9c51fb552ad21d4d3750c5133b4
David Futcher <david.futcher@skyscanner.net>
2018-12-06 19:27:17 +0000

    Simple debug database setup

---
+++ src/database.js
@@ -0,0 +1,44 @@
+import { Sequelize } from 'sequelize';
+
+class Database {
+
+  async connect() {
+    this.db = new Sequelize('sqlite:./records.rack');
+    this.Record = this.db.define('record', {
+      id: { type: Sequelize.DECIMAL, primaryKey: true },
+      name: Sequelize.STRING,
+      artist: Sequelize.STRING,
+    });
+
+    return this.Record.sync();
+  }
+
+  async ensureDebugData() {
+    const debugRecords = [
+      {
+        id: 1,
+        artist: 'Los Campesinos!',
+        name: 'Hello Sadness',
+      },
+      {
+        id: 2,
+        artist: 'The Antlers',
+        name: 'Hospice',
+      },
+    ];
+
+    const creates = [];
+    for (const record of debugRecords) {
+      creates.push(this.Record.create(record));
+    }
+
+    Promise.all(creates);
+  }
+
+  async getAllRecords() {
+    return this.Record.findAll();
+  }
+
+}
+
+export default Database;
